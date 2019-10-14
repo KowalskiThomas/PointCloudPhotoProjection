5468b6f29192eff41580115726c25d95acd28c9e
Thomas Kowalski <thom.kowa@gmail.com>
2019-10-14 20:32:46 +0100

    Added output directory creation if it does not exist

--- render.cpp
+++ render.cpp
@@ -2,6 +2,9 @@
 #include "animator.h"
 
 int main() {
+    if (!fs::is_directory("output"))
+        fs::create_directories("output");
+    assert(fs::is_directory("output"));
     auto anim = animator("/Users/kowalski/Desktop/Imperial/Projet/projection/data");
     anim.create_animation("output");
     return 0;
