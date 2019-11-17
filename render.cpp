9a58d7b4ce354ca565088b1003d89229ee6e0a11
Thomas Kowalski <thom.kowa@gmail.com>
2019-11-17 22:49:43 +0000

    Custom data directory from command line arguments

--- render.cpp
+++ render.cpp
@@ -1,8 +1,19 @@
 #include "defines.h"
 #include "animator.h"
 
-int main() {
-    auto anim = animator("/Users/kowalski/Desktop/Imperial/Projet/projection/data");
+int main(int argc, char** argv) {
+    std::string path = "/Users/kowalski/Desktop/Imperial/Projet/projection/data";
+    if (argc > 1)
+        path = argv[1];
+    
+    std::cout << "Taking images and points from " << path << std::endl;
+    
+    if (!fs::exists(path)) {
+        std::cerr << "The directory doesn't exist!" << std::endl;
+        return 1;
+    }
+
+    auto anim = animator(path);
     anim.create_animation("output");
     return 0;
 }
